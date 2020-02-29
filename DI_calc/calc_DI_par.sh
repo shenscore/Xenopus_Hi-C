05594d4b8e6eb7cd0f884b0ab083ffa3775fc3fa
shen <490788573@qq.com>
2020-02-29 11:07:18 +0800

    Update calc_DI_par.sh

--- DI_calc/calc_DI_par.sh
+++ DI_calc/calc_DI_par.sh
@@ -9,17 +9,20 @@ threads=$3
 chrList=(Chr01 Chr02 Chr03 Chr04 Chr05 Chr06 Chr07 Chr08 Chr09 Chr10)
 block_size=40
 
-dir=/stort/wshen/xenopus_hic/DI_index
+dir=/path/to/script
 
 name=$(basename $hic_file .hic)
 
 
 for chr in ${chrList[*]}
 do
-#  echo "fixedStep chrom=$chr start=1 step="${res}" span="${res} > $name.$chr.border_strength.wig
-#  straw KR $hic_file $chr $chr BP $res | ${dir}/calc_DI.awk -v block_size=$block_size -v res=$res >> $name.$chr.DI
-  straw KR $hic_file $chr $chr BP $res | parallel --pipe -j $threads ${dir}/calc_DI_worker.awk -v block_size=$block_size -v res=$res | ${dir}/calc_DI_merge.awk > $name.$chr.DI
+  echo "fixedStep chrom=$chr start=1 step="${res}" span="${res} > $name.$chr.DI.wig
+  straw VC_SQRT $hic_file $chr $chr BP $res | parallel --pipe -j $threads ${dir}/calc_DI_worker.awk -v block_size=$block_size -v res=$res | ${dir}/calc_DI_merge.awk >> $name.$chr.DI.wig
 done
 
-#cat $name.*.DI >$name.all.DI
+cat $name.*.insulation.wig >$name.all.DI.wig
 
+for chr in ${chrList[*]}
+do
+    rm $name.$chr.DI.wig
+done
